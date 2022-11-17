51e04fd5a4d08116d299d056b1ca9a03ab3226a3
xbugster <leaderpvp@gmail.com>
2022-11-17 11:38:59 +0200

    returned config pull url key in routes definition.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 use PayMe\Remotisan\Http\Controllers\RemotisanController;
 
 Route::middleware('web')
-    ->prefix(config("remotisan.base_url_prefix"))->group(function () {
+    ->prefix(config("remotisan.url"))->group(function () {
     Route::get('/', [RemotisanController::class, "index"]);
     Route::get('/commands', [RemotisanController::class, "commands"]);
     Route::post('/execute', [RemotisanController::class, "execute"]);
