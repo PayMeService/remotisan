c082806655e8cd36e10a0099a06e9ba8c009a17c
xbugster <leaderpvp@gmail.com>
2023-04-04 12:23:21 +0300

    re-adjustments to break down functionality and re-application of pcntl.

--- src/Console/Commands/ProcessBrokerCommand.php
+++ src/Console/Commands/ProcessBrokerCommand.php
@@ -14,9 +14,10 @@ use PayMe\Remotisan\CacheManager;
 use PayMe\Remotisan\FileManager;
 use PayMe\Remotisan\Models\Execution;
 use PayMe\Remotisan\Remotisan;
+use Symfony\Component\Console\Command\SignalableCommandInterface;
 use Symfony\Component\Process\Process;
 
-class ProcessBrokerCommand extends Command
+class ProcessBrokerCommand extends Command implements SignalableCommandInterface
 {
     /**
      * The name and signature of the console command.
@@ -32,49 +33,126 @@ class ProcessBrokerCommand extends Command
      */
     protected $description = "Remotisan's jobs broker";
 
+    protected bool $isKilled = false;
+
+    protected bool $isErroneous = false;
+
+    protected string $pathToLog;
+
+    protected Remotisan $remotisan;
+
+    protected Process $process;
+
+    protected Execution $executionRecord;
+
+    protected array $killSignalsList = [SIGQUIT, SIGINT, SIGTERM, SIGHUP];
+
     public function handle(Remotisan $remotisan)
     {
-        $isProcessKilled = false;
-        $isProcessErroneous = false;
-        $executionRecord = Execution::getByJobUuid($this->argument("uuid"));
-        $pathToLog = FileManager::getLogFilePath($executionRecord->job_uuid);
-        $commandArray = array_merge(
-            explode(' ', "php artisan " . $executionRecord->command),
-            $remotisan->getProcessExecutor()->compileCmdAsEscapedArray($executionRecord->parameters)
-        );
+        $this->remotisan = $remotisan;
+        $this->executionRecord = Execution::getByJobUuid($this->argument("uuid"));
+        $this->pathToLog = FileManager::getLogFilePath($this->executionRecord->job_uuid);
 
-        $process = new Process($commandArray, base_path());
-        $process->start();
+        $exitCode = $this->executeProcess($this->executionRecord);
+        $this->postExecutionProcessing($this->executionRecord, $exitCode);
+    }
 
-        $exitCode = $process->wait(function ($type, $buffer) use (&$isProcessKilled, &$isProcessErroneous, $executionRecord, $pathToLog, &$process) {
-            file_put_contents($pathToLog, $buffer, FILE_APPEND);
-            if ($process->isRunning() && CacheManager::hasKillInstruction($executionRecord->job_uuid)) {
-                $isProcessKilled = true;
-                $process->signal(9);
+    /**
+     * Executes the command, first building using buildCommandArray. Writes into log and returns exit code
+     *
+     *
+     * @param Execution $executionRecord
+     * @return int exitCode
+     */
+    protected function executeProcess(Execution $executionRecord): int
+    {
+        $this->process = new Process($this->buildCommandArray($executionRecord), base_path());
+        $this->process->setTimeout(null);
+        $this->process->start();
+        $sigTime = 0;
+        $signals = $this->killSignalsList;
+
+        return $this->process->wait(function ($type, $buffer) use ($executionRecord, &$sigTime, &$signals) {
+            file_put_contents($this->pathToLog, $buffer, FILE_APPEND);
+            if ($this->process->isRunning() && CacheManager::hasKillInstruction($executionRecord->job_uuid)) {
+                $nowTime = time();
+                if($sigTime+5 < $nowTime) {
+                    $this->isKilled = true;
+                    $this->process->signal((!empty($signals) ? array_shift($signals) : SIGKILL));
+                }
             }
 
             if (Process::ERR === $type) {
-                $isProcessErroneous = true;
+                $this->isErroneous = true;
             }
         });
+    }
 
-        $executionRecord->refresh();
+    /**
+     * Building array of command for Process to use.
+     *
+     * @param Execution $executionRecord
+     * @return array
+     */
+    protected function buildCommandArray(Execution $executionRecord): array
+    {
+        return array_merge(
+            explode(' ', "php artisan " . $executionRecord->command),
+            $this->remotisan->getProcessExecutor()->compileCmdAsEscapedArray($executionRecord->parameters)
+        );
+    }
 
-        if ($isProcessKilled) {
-
-            $killNote = "\n=============
+    /**
+     * Post Kill actions.
+     *      (1) Append kill note to log
+     *      (2) remove instruction from cache
+     *      (3) mark execution record killed
+     *
+     * @param Execution $executionRecord
+     * @return void
+     */
+    protected function postKill(Execution $executionRecord): void
+    {
+        $killNote = "\n=============
             \nPROCESS KILLED BY {$executionRecord->killed_by} AT " . ((string)Carbon::parse()) . " \n";
-            file_put_contents($pathToLog, $killNote, FILE_APPEND);
-            CacheManager::removeKillInstruction($executionRecord->job_uuid);
-            $executionRecord->markKilled();
+        file_put_contents($this->pathToLog, $killNote, FILE_APPEND);
+        CacheManager::removeKillInstruction($executionRecord->job_uuid);
+        $executionRecord->markKilled();
+    }
+
+    /**
+     * Post execution processing responsible for processing exitCode and process results, then act upon it.
+     * Flag record killed, failed and completed.
+     *
+     * @param Execution $executionRecord
+     * @param int $exitCode
+     * @return void
+     */
+    protected function postExecutionProcessing(Execution $executionRecord, int $exitCode)
+    {
+        $executionRecord->refresh();
+        if ($this->isKilled) {
+            $this->postKill($executionRecord);
 
             return;
         }
 
-        if (($isProcessErroneous && $exitCode != 0) || !$process->isSuccessful()) {
+        if ($this->isErroneous  || !$this->process->isSuccessful()) {
             $executionRecord->markFailed();
         } else {
             $executionRecord->markCompleted();
         }
+    }
+
+    public function getSubscribedSignals(): array
+    {
+        return $this->killSignalsList;
+    }
+
+    public function handleSignal(int $signal): void
+    {
+        $this->isKilled = true;
+        $this->process->signal($signal);
+        $this->postKill($this->executionRecord);
     }
 }
