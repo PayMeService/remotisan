803afebfaf1ac1226cb847f3c6900d6bcc8e2b34
xbugster <leaderpvp@gmail.com>
2023-05-23 18:13:38 +0300

    added list of sig consts with values, as Omer wrote.

--- src/RemotisanServiceProvider.php
+++ src/RemotisanServiceProvider.php
@@ -28,6 +28,8 @@ class RemotisanServiceProvider extends ServiceProvider
 
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
+        $this->ensureSignalsConstants();
+
         $this->commands([ProcessBrokerCommand::class]);
     }
 
@@ -44,5 +46,55 @@ class RemotisanServiceProvider extends ServiceProvider
         $this->app->singleton(Remotisan::class, function ($app) {
             return new Remotisan(new CommandsRepository(), new ProcessExecutor());
         });
+    }
+
+    protected function ensureSignalsConstants(): void
+    {
+        $consts = [
+            'SIG_IGN' => 1,
+            'SIG_DFL' => 0,
+            'SIG_ERR' => -1,
+            'SIGHUP' => 1,
+            'SIGINT' => 2,
+            'SIGQUIT' => 3,
+            'SIGILL' => 4,
+            'SIGTRAP' => 5,
+            'SIGABRT' => 6,
+            'SIGIOT' => 6,
+            'SIGBUS' => 7,
+            'SIGFPE' => 8,
+            'SIGKILL' => 9,
+            'SIGUSR1' => 10,
+            'SIGSEGV' => 11,
+            'SIGUSR2' => 12,
+            'SIGPIPE' => 13,
+            'SIGALRM' => 14,
+            'SIGTERM' => 15,
+            'SIGSTKFLT' => 16,
+            'SIGCLD' => 17,
+            'SIGCHLD' => 17,
+            'SIGCONT' => 18,
+            'SIGSTOP' => 19,
+            'SIGTSTP' => 20,
+            'SIGTTIN' => 21,
+            'SIGTTOU' => 22,
+            'SIGURG' => 23,
+            'SIGXCPU' => 24,
+            'SIGXFSZ' => 25,
+            'SIGVTALRM' => 26,
+            'SIGPROF' => 27,
+            'SIGWINCH' => 28,
+            'SIGPOLL' => 29,
+            'SIGIO' => 29,
+            'SIGPWR' => 30,
+            'SIGSYS' => 31,
+            'SIGBABY' => 31,
+        ];
+
+        foreach ($consts as $const => $value) {
+            if (!defined($const)) {
+                define($const, $value);
+            }
+        }
     }
 }
